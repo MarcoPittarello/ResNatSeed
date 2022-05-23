f52d6096f4c827d02eb06908e948ec5f15968168
MarcoPittarello <marco.pittarello1987@yahoo.it>
2022-05-23 11:57:32 +0200

    vignette updates

---
+++ data-raw/File nascosti.R
@@ -0,0 +1,6 @@
+## code to prepare `env.veg.sel.new` dataset goes here
+
+#caricare il file trainingPie.resnatseed e far girare questo
+
+usethis::use_data(trainingPie.resnatseed,veg.composition.example, internal = T,overwrite = T)
+
